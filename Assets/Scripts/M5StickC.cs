f7ca2cf54cfb3f19260f4c9ee8432c5dff6351c7
machidyo <yorihiko_m@hotmail.com>
2021-03-13 12:00:51 +0900

    first commit

---
+++ Assets/Scripts/M5StickC.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class M5StickC : MonoBehaviour
+{
+    [SerializeField] private UDPReceiver receiver;
+
+    private bool isRyap = true;
+    private Quaternion baseQuaternion = Quaternion.identity;
+
+    void Update()
+    {
+        if (receiver.AhrsData != null)
+        {
+            if (isRyap)
+            {
+                UpdateQuaternion(receiver.AhrsData);
+            }
+            else
+            {
+                var x = receiver.AhrsData[0];
+                var y = receiver.AhrsData[2];
+                var z = receiver.AhrsData[1];
+                transform.rotation = Quaternion.Euler(x, y, z);
+            }
+        }
+    }
+
+    private void UpdateQuaternion(float[] raw)
+    {
+        var quaternion = new Quaternion(raw[1], raw[3], raw[2], -raw[0]).normalized;
+
+        if (baseQuaternion == Quaternion.identity) {
+            baseQuaternion = quaternion;
+        }
+        
+        transform.rotation = Quaternion.Inverse(baseQuaternion) * quaternion;
+    }
+}
---
+++ Assets/Scripts/UDPReceiver.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Collections;
+
+public class UDPReceiver : MonoBehaviour
+{
+    private const int LOCAL_PORT = 22222;
+    private const int DATA_NUMBER = 4;
+
+    public float[] AhrsData = new float[DATA_NUMBER];
+
+    private UdpClient udp;
+
+    void Start()
+    {
+        Debug.Log("START Start");
+        udp = new UdpClient(LOCAL_PORT) {Client = {ReceiveTimeout = 100}};
+        StartCoroutine(ThreadMethod());
+    }
+
+    private IEnumerator ThreadMethod()
+    {
+        while (true)
+        {
+            IPEndPoint remoteEP = null;
+            var data = udp.Receive(ref remoteEP);
+            Read(data);
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+
+    // wifi のやつのところからひっぱってきたの
+    private void ReadThreeFloatData(byte[] data)
+    {
+        if (data.Length == DATA_NUMBER * 4) // float または Int32 のサイズとして 4
+        {
+            for (var i = 0; i < DATA_NUMBER; i++)
+            {
+                AhrsData[i] = BitConverter.ToSingle(data, i * sizeof(float));
+            }
+
+            // Debug.Log(string.Join(", ", AhrsData));
+        }
+    }
+
+    private void Read(byte[] data)
+    {
+        AhrsData = data.ToQuaternion(0);
+        // var t = data.ToUInt(0);
+        // var acc = data.ToVector3(4);
+        // var gyro = data.ToVector3(16);
+        // var quat = data.ToQuaternion(28);
+    }
+}
