728bcd9d42e4b01919df9e3925f86a15ddc85c3f
machidyo <yorihiko_m@hotmail.com>
2021-03-13 18:02:39 +0900

    refactor:不要なコードを削除したり、必要な変数を外に出したり内にしまったり、全体的に整理した

--- Assets/Scripts/ByteArrayConvertEx.cs
+++ Assets/Scripts/ByteArrayConvertEx.cs
@@ -2,40 +2,17 @@
 
 internal static class ByteArrayConvertEx
 {
-    public static uint ToUInt(this byte[] byteArray, int offset)
-    {
-        if (byteArray == null)
-        {
-            return 0;
-        }
-
-        return BitConverter.ToUInt32(byteArray, offset);
-    }
-
-    public static float[] ToVector3(this byte[] byteArray, int offset)
-    {
-        if (byteArray == null)
-        {
-            return new float[] {0.0F, 0.0F, 0.0F};
-        }
-
-        var x = BitConverter.ToSingle(byteArray, offset);
-        var y = BitConverter.ToSingle(byteArray, offset + 4);
-        var z = BitConverter.ToSingle(byteArray, offset + 8);
-        return new float[] {x, y, z};
-    }
-
     public static float[] ToQuaternion(this byte[] byteArray, int offset)
     {
         if (byteArray == null)
         {
-            return new float[] {1.0F, 0.0F, 0.0F, 0.0F};
+            return new[] {1.0F, 0.0F, 0.0F, 0.0F};
         }
 
         var w = BitConverter.ToSingle(byteArray, offset);
         var x = BitConverter.ToSingle(byteArray, offset + 4);
         var y = BitConverter.ToSingle(byteArray, offset + 8);
         var z = BitConverter.ToSingle(byteArray, offset + 12);
-        return new float[] {w, x, y, z};
+        return new[] {w, x, y, z};
     }
 }
--- Assets/Scripts/M5StickC.cs
+++ Assets/Scripts/M5StickC.cs
@@ -2,26 +2,19 @@
 
 public class M5StickC : MonoBehaviour
 {
-    [SerializeField] private UDPReceiver receiver;
-
-    private bool isRyap = true;
+    private UDPReceiver receiver;
     private Quaternion baseQuaternion = Quaternion.identity;
+
+    void Start()
+    {
+        receiver = FindObjectOfType<UDPReceiver>();
+    }
 
     void Update()
     {
         if (receiver.AhrsData != null)
         {
-            if (isRyap)
-            {
-                UpdateQuaternion(receiver.AhrsData);
-            }
-            else
-            {
-                var x = receiver.AhrsData[0];
-                var y = receiver.AhrsData[2];
-                var z = receiver.AhrsData[1];
-                transform.rotation = Quaternion.Euler(x, y, z);
-            }
+            UpdateQuaternion(receiver.AhrsData);
         }
     }
 
@@ -29,10 +22,11 @@ public class M5StickC : MonoBehaviour
     {
         var quaternion = new Quaternion(raw[1], raw[3], raw[2], -raw[0]).normalized;
 
-        if (baseQuaternion == Quaternion.identity) {
+        if (baseQuaternion == Quaternion.identity)
+        {
             baseQuaternion = quaternion;
         }
-        
+
         transform.rotation = Quaternion.Inverse(baseQuaternion) * quaternion;
     }
 }
